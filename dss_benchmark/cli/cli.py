1873be183958b6759f61e773bb25838492006508
SqrtMinusOne <thexcloud@gmail.com>
2023-06-17 10:45:02 +0300

    benchmarks: add basic stats

--- dss_benchmark/cli/cli.py
+++ dss_benchmark/cli/cli.py
@@ -1,6 +1,7 @@
 import click
 from dss_benchmark.cli.methods import kwm
 from dss_benchmark.cli.experiments import kwme
+from dss_benchmark.cli.experiments import benchmarks
 
 __all__ = ["cli"]
 
@@ -12,6 +13,7 @@ def cli():
 
 cli.add_command(kwm)
 cli.add_command(kwme)
+cli.add_command(benchmarks)
 
 if __name__ == "__main__":
     cli()
