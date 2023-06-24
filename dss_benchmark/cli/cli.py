5de0507513c0585165822c0d4694e17fdced817a
anmatveev <andrewsamsunguser@gmail.com>
2023-06-24 22:23:46 +0300

    done

--- dss_benchmark/cli/cli.py
+++ dss_benchmark/cli/cli.py
@@ -19,18 +19,13 @@ def cli():
 
 cli.add_command(kwm)
 cli.add_command(kwme)
-<<<<<<< HEAD
-cli.add_command(trn)
-cli.add_command(mch)
-cli.add_command(rsch)
-
-cli.add_command(benchmarks)
-cli.add_command(benchmarks)
-=======
 cli.add_command(benchmarks)
 cli.add_command(gptme)
 cli.add_command(gpt)
->>>>>>> 40832e5fe0690735883dfea59a5fda3f6e178392
+
+cli.add_command(trn)
+cli.add_command(mch)
+cli.add_command(rsch)
 
 if __name__ == "__main__":
     cli()
